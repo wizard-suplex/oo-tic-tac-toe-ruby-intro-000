debb70f37560ac9a53d5646fc68c34f1172b4f80
Alan Cooke <atc4@rice.edu>
2021-01-20 20:24:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
 
   # Make sure the move is valid
   def valid_move?(index)
-    !position_taken?(index) && index.between(0,8)
+    !position_taken?(index) && index.between?(0,8)
   end
 
   # Make a move
